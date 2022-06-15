e5a2440eb04029671bdb75ae3b2e8194d5ee2ee4
Michaelmw17 <Michaelmw17@outlook.com>
2022-06-15 13:58:16 +1000

    Minior fix for moblie pop up

--- components/TopSection.js
+++ components/TopSection.js
@@ -64,7 +64,7 @@ const TopSection = () => {
                   position: 'absolute',
                   left: '50%',
                   top: '50%',
-                  transform: 'translate(-50%, -50%)',
+                  transform: 'translate(-55%, -50%)',
                 }}
                 open
                 onClick={handleShowDialog}
